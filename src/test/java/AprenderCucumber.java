5f8cffd775bef5b74829627246de93cf92c03305
Isabella Henriques Szrajbman <isabella.henriques@hotmail.com>
2022-03-09 14:27:43 -0300

    cenário contador
    Aula 10 cenário contador

--- src/test/java/AprenderCucumber.java
+++ src/test/java/AprenderCucumber.java
@@ -1,5 +1,6 @@
 import cucumber.api.java.pt.Dado;
 import cucumber.api.java.pt.Entao;
+import cucumber.api.java.pt.Então;
 import cucumber.api.java.pt.Quando;
 
 public class AprenderCucumber {
@@ -14,5 +15,24 @@ public class AprenderCucumber {
 
     @Entao("^a especificacao deve finalizar com sucesso$")
     public void aEspecificacaoDeveFinalizarComSucesso() throws Throwable {
+    }
+
+    private int contador = 0;
+
+    @Dado("^que o valor do contador é (\\d+)$")
+    public void queOValorDoContadorÉ(int arg1) throws Throwable {
+        contador = arg1;
+    }
+
+    @Quando("^eu incrementar em (\\d+)$")
+    public void euIncrementarEm(int arg1) throws Throwable {
+        contador = contador + arg1;
+    }
+
+    @Então("^o valor do contador será (\\d+)$")
+    public void oValorDoContadorSerá(int arg1) throws Throwable {
+        System.out.println(arg1);
+        System.out.println(contador);
+        System.out.println(arg1 == contador);
     }
 }
--- src/test/resources/aprendercucumber/aprender_cucumber.feature
+++ src/test/resources/aprendercucumber/aprender_cucumber.feature
@@ -8,4 +8,8 @@ Cenario: Deve executar especificacao
   Dado que criei o arquivo corretamente
   Quando executaLo
   Entao a especificacao deve finalizar com sucesso
-  Entao outro passo qualquer
+
+Cenario: Deve incrementar contador
+  Dado que o valor do contador é 15
+  Quando eu incrementar em 5
+  Então o valor do contador será 18
