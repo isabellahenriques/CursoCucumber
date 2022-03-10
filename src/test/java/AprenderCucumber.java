e28ab5de1bfc52fe921c5da39900a018f02fc7bc
Isabella Henriques Szrajbman <isabella.henriques@hotmail.com>
2022-03-10 10:24:33 -0300

    Desafio REGEX
    Aula 14

--- src/test/java/AprenderCucumber.java
+++ src/test/java/AprenderCucumber.java
@@ -75,4 +75,30 @@ public class AprenderCucumber {
         String dataFormatada = format.format(entrega);
         Assert.assertEquals(data, dataFormatada);
     }
+
+    @Dado("^que o ticket( especial)? é (A.\\d{3})$")
+    public void queOTicketÉAF(String tipo, String arg1) throws Throwable {
+    }
+
+    @Dado("^que o valor da passagem é R\\$ (\\d+),(\\d+)$")
+    public void queOValorDaPassagemÉR$(int arg1, int arg2) throws Throwable {
+    }
+
+    @Dado("^que o nome do passageiro é \"(.{5,20})\"$")
+    public void queONomeDoPassageiroÉ(String arg1) throws Throwable {
+    }
+
+    @Dado("^que o telefone do passageiro é (9\\d{3}-\\d{4})$")
+    public void queOTelefoneDoPassageiroÉ(String telefone) throws Throwable {
+    }
+
+    @Dado("^criar os steps$")
+    public void criarOsSteps() throws Throwable {
+    }
+
+    @Dado("^o teste vai funcionar$")
+    public void oTesteVaiFuncionar() throws Throwable {
+    }
+
+
 } // Não apagar
