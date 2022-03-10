7b4f72ec4087cdb80419e35dfd576aac4b17960c
Isabella Henriques Szrajbman <isabella.henriques@hotmail.com>
2022-03-10 09:36:50 -0300

    Reaproveitando cenários - Regex
    Aula 12

--- src/test/java/AprenderCucumber.java
+++ src/test/java/AprenderCucumber.java
@@ -1,8 +1,17 @@
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+import cucumber.api.Transform;
 import cucumber.api.java.pt.Dado;
 import cucumber.api.java.pt.Entao;
 import cucumber.api.java.pt.Então;
 import cucumber.api.java.pt.Quando;
+import cucumber.deps.com.thoughtworks.xstream.converters.basic.DateConverter;
 import org.junit.Assert;
+
+import java.util.Date;
 
 public class AprenderCucumber {
     @Dado("^que criei o arquivo corretamente$")
@@ -35,4 +44,35 @@ public class AprenderCucumber {
         Assert.assertEquals(arg1, contador);
 //        throw new RuntimeException();
     }
-}
+
+    Date entrega = new Date();
+
+    @Dado("^que a entrega e dia (\\d+)/(\\d+)/(\\d+)$")
+    public void queAEntregaEDia(int dia, int mes, int ano) throws Throwable {
+        Calendar cal = Calendar.getInstance();
+        cal.set(Calendar.DAY_OF_MONTH, dia);
+        cal.set(Calendar.MONTH, mes -1);
+        cal.set(Calendar.YEAR, ano);
+        entrega = cal.getTime();
+    }
+
+    @Quando("^a entrega atrasar em (\\d+) (.+)$")
+    public void aEntregaAtrasarEmDias(int arg1, String tempo) throws Throwable {
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(entrega);
+        if(tempo.equals("dias")){
+            cal.add(Calendar.DAY_OF_MONTH, arg1);
+        }
+        if(tempo.equals("meses")){
+            cal.add(Calendar.MONTH, arg1);
+        }
+        entrega = cal.getTime();
+    }
+
+    @Então("^a entrega sera efetuada em (\\d{2}\\/\\d{2}\\/\\d{4})$")
+    public void aEntregaSeraEfetuadaEm(String data) throws Throwable {
+        DateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+        String dataFormatada = format.format(entrega);
+        Assert.assertEquals(data, dataFormatada);
+    }
+} // Não apagar
